94f31925b9ebb1c49a206235f342a971b3108fb9
Artem-Marakhovskyi <Artem.marakhovskyi@gmail.com>
2021-01-11 18:53:54 +0200

    lb 1 is done

---
+++ lexer.py
@@ -0,0 +1,78 @@
+import re
+
+
+class Lexeme:
+    def __init__(self, marker, value):
+        self.marker = marker
+        self.value = value
+
+    def __str__(self):
+        return '[ %s -> %s]' % (self.marker, self.value)
+
+
+class Lexer:
+    def analyze(self, code: str):
+        while len(code) > 0:
+            code, lexeme = self.__find_next_lexeme(code)
+            self.lexemes.append(lexeme)
+        return self.lexemes.copy()
+
+    def __init__(self):
+        self.rules = []
+        self.lexemes = []
+        self.__insert_key_words()
+        self.__insert_key_symbols()
+        self.__insert_arithmetical_operations()
+        self.__insert_assignment()
+        self.__insert_variable()
+
+    def __insert_key_words(self):
+        self.__append_lexemes(
+            ('cycle', 'for'),
+            ('cond', 'if'),
+            ('begin', 'block-start'),
+            ('end', 'block-end'),
+            ('eq', '=='),
+            ('less', '<'),
+            ('greater', '>'),
+            ('less_eq', '<='),
+            ('greater_eq', '>='),
+            ('false|False', 'Boolean False'),
+            ('true|True', 'Boolean True'))
+
+    def __insert_key_symbols(self):
+        self.__append_lexemes(
+            ('{', 'bracket_start'),
+            ('}', 'bracket_end'),
+            ('\(', 'parenthesis_start'),
+            ('\)', 'parenthesis_end'),
+            ('\,', 'comma'))
+
+    def __insert_arithmetical_operations(self):
+        self.__append_lexemes(
+            ('\d+', 'number'),
+            ('plu', 'plus'),
+            ('min', 'minus'),
+            ('mul', 'mult'),
+            ('div', 'div'),
+            ('inc', '++'))
+
+    def __insert_assignment(self):
+        self.__append_lexemes(
+            ('assign', '='))
+
+    def __insert_variable(self):
+        self.__append_lexemes(
+            ('[a-zA-Z]+[a-zA-Z0-9]?', 'variable_name'))
+
+    def __append_lexemes(self, *regexes):
+        for x in regexes:
+            self.rules.append((re.compile(x[0]), x[1]))
+
+    def __find_next_lexeme(self, code) -> (str, Lexeme):
+        code = code.strip()
+        for rule in self.rules:
+            m = rule[0].match(code)
+            if m is not None:
+                return code[m.span()[1]:], Lexeme(rule[1], code[:m.span()[1]])
+        return code[1:], Lexeme('error', code[:1])
---
+++ main.py
@@ -0,0 +1,16 @@
+import lexer
+
+def read_and_run_lexer(filename = None):
+    return lexer.Lexer().analyze(get_file_content(filename))
+
+
+def get_file_content(filename):
+    file = open(filename)
+    return ''.join(file.readlines())
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    filename = 'lang_samples/if-error.alng'
+    print(get_file_content(filename))
+    for x in read_and_run_lexer(filename):
+        print(x)
